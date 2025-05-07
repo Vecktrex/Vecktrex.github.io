7a48b8a0f660cbbd4c1c9966f16451ef3ee5e040
vecktrex <vectrex720@gmail.com>
2025-05-07 15:52:03 -0400

    Updated index.html, db functionality test in process.php

--- process.php
+++ process.php
@@ -1,8 +1,27 @@
 <?php
 
-$name = $_GET["fullname"];
-$email = $_GET["email"];
+$sku = $_POST["sku"];
+$itemName = $_POST["itemName"];
+$itemQuantity = $_POST["quantity"];
+$itemCost = $_POST["itemCost"];
+$sellPrice = $_POST["sellPrice"];
 
-echo "<p>$name has an email address of $email</p>";
+$dsn = "mysql:host=sql113.infinityfree.com;dbname=if0_38721731_cis224";
+$username = "if0_38721731";
+$password = "0L2slKW8df";
+$pdo = new PDO($dsn, $username, $password);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+$sql = "INSERT INTO Inventory VALUES ('$sku', '$itemName', '$itemQuantity', '$itemCost', '$sellPrice')";
+$statement = $pdo->prepare($sql);
+$params = [$sku, $itemName, $itemQuantity, $itemCost, $sellPrice];
+$statement->execute($params);
+echo "$itemName was entered into the system.";
+
+} catch (PDOException $e) {
+echo $e;
+}
 
 ?>
